28d089bbf31a77e8b0ac0b3d3b0ed5b4982a1cd5
HuangJianMei <fz-c@qq.com>
2022-06-30 17:16:48 +0800

    补上代码

---
+++ src/main/java/com/cfz/gitstudy/GitStudyApplication.java
@@ -0,0 +1,13 @@
+package com.cfz.gitstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cfz/gitstudy/GitStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfz.gitstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
